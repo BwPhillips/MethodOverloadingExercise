e283be1961123d4cc93034402df23e568d59fba4
Brenton Phillips <phillips.brenton@gmail.com>
2020-06-24 15:09:17 -0500

    Method Overloading Exercise

---
+++ MethodOverloadingConsol/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MethodOverloadingConsol
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
